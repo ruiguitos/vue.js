e6ce936ea682724a0fb17addc0bf2d486981505c
ruiguitos <ruiguitos@hotmail.com>
2022-11-16 11:28:31 +0000

    avatar validator

--- src/pages/PageRegister.vue
+++ src/pages/PageRegister.vue
@@ -2,15 +2,15 @@
   <div class="flex-grid justify-center">
     <div class="col-2">
 
-      <form @submit.prevent="register" class="card card-form">
+      <form class="card card-form" @submit.prevent="register">
         <h1 class="text-center">Register</h1>
 
         <div class="form-group">
           <label for="name">Full Name</label>
           <input
+            id="name"
             v-model="form.name"
-            @blur="$v.form.name.$touch()"
-            id="name" type="text" class="form-input">
+            class="form-input" type="text" @blur="$v.form.name.$touch()">
           <template v-if="$v.form.name.$error">
             <span v-if="!$v.form.name.required" class="form-error">This field is required</span>
           </template>
@@ -19,9 +19,9 @@
         <div class="form-group">
           <label for="username">Username</label>
           <input
+            id="username"
             v-model.lazy="form.username"
-            @blur="$v.form.username.$touch()"
-            id="username" type="text" class="form-input">
+            class="form-input" type="text" @blur="$v.form.username.$touch()">
           <template v-if="$v.form.username.$error">
             <span v-if="!$v.form.username.required" class="form-error">This field is required</span>
             <span v-if="!$v.form.username.unique" class="form-error">Sorry! This username is taken</span>
@@ -31,9 +31,9 @@
         <div class="form-group">
           <label for="email">Email</label>
           <input
+            id="email"
             v-model.lazy="form.email"
-            @blur="$v.form.email.$touch()"
-            id="email" class="form-input">
+            class="form-input" @blur="$v.form.email.$touch()">
           <template v-if="$v.form.email.$error">
             <span v-if="!$v.form.email.required" class="form-error">This field is required</span>
             <span v-else-if="!$v.form.email.email" class="form-error">This in not a valid email address</span>
@@ -45,33 +45,38 @@
         <div class="form-group">
           <label for="password">Password</label>
           <input
+            id="password"
             v-model="form.password"
-            @blur="$v.form.password.$touch()"
-            id="password" type="password" class="form-input">
+            class="form-input" type="password" @blur="$v.form.password.$touch()">
           <template v-if="$v.form.password.$error">
             <span v-if="!$v.form.password.required" class="form-error">This field is required</span>
-            <span v-if="!$v.form.password.minLength" class="form-error">The password must be at least 6 characters long</span>
+            <span v-if="!$v.form.password.minLength"
+                  class="form-error">The password must be at least 6 characters long</span>
           </template>
         </div>
 
         <div class="form-group">
           <label for="avatar">Avatar</label>
           <input
-            v-model="form.avatar"
-            @blur="$v.form.avatar.$touch()"
-            id="avatar" type="text" class="form-input">
+            id="avatar"
+            v-model.lazy="form.avatar"
+            class="form-input" type="text" @blur="$v.form.avatar.$touch()">
           <template v-if="$v.form.avatar.$error">
-
+            <span v-if="!$v.form.avatar.url" class="form-error">The supplied URL is invalid</span>
+            <span v-if="!$v.form.avatar.supportedImageFile" class="form-error">This file type is not supported by our system</span>
+            <span v-else-if="!$v.form.avatar.responseOk" class="form-error">The supplied image cannot be found</span>
           </template>
         </div>
 
         <div class="form-actions">
-          <button type="submit" class="btn-blue btn-block">Register</button>
+          <button class="btn-blue btn-block" type="submit">Register</button>
         </div>
 
       </form>
       <div class="text-center push-top">
-        <button @click="registerWithGoogle" class="btn-red btn-xsmall"><i class="fa fa-google fa-btn"></i>Sign up with Google</button>
+        <button class="btn-red btn-xsmall" @click="registerWithGoogle"><i class="fa fa-google fa-btn"></i>Sign up with
+          Google
+        </button>
       </div>
     </div>
   </div>
@@ -81,7 +86,8 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/database'
-import {required, email, minLength, helpers as vuelidateHelpers} from 'vuelidate/lib/validators'
+import {email, helpers as vuelidateHelpers, minLength, required, url} from 'vuelidate/lib/validators'
+
 export default {
   data () {
     return {
@@ -129,6 +135,25 @@ export default {
         minLength: minLength(6)
       },
       avatar: {
+        url,
+        supportedImageFile (value) {
+          if (!vuelidateHelpers.req(value)) {
+            return true
+          }
+          const supported = ['jpg', 'jpeg', 'gif', 'png', 'svg']
+          const suffix = value.split('.').pop()
+          return supported.includes(suffix)
+        },
+        responseOk (value) {
+          if (!vuelidateHelpers.req(value)) {
+            return true
+          }
+          return new Promise((resolve, reject) => {
+            fetch(value)
+              .then(response => resolve(response.ok))
+              .catch(() => resolve(false))
+          })
+        }
       }
     }
   },
